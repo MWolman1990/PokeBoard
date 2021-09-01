e29c266290d39928c44470292ebf92659bf32aa3
Matt <matthew.wolman@gmail.com>
2021-08-31 21:58:39 -0600

    Added static usage to express app to serve create-react-app

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const pokemonRouter = require('./api/pokemon-api')
 const typeRouter = require('./api/type-api')
 app.use('/api/pokemon', pokemonRouter)
 app.use('/api/type', typeRouter)
-
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'poke-board/build')));
+        
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'poke-board/build', 'index.html'));
+    });
+}
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
