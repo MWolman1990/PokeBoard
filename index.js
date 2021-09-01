c68746328eb6a55dca06834f602c313242411ac6
Matt <matthew.wolman@gmail.com>
2021-08-31 22:02:17 -0600

    Added path variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const request = require('request')
 const PORT = process.env.PORT || 5000
-
+const path = require('path')
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
